062ab15993537e8dad32c88ce721bc8b71d0c00b
20120509 <topsiu1.ds@gmail.com>
2023-11-18 12:08:18 +0700

    demo sign in

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -2,7 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import DefaultLayout from "../layout/DefaultLayout";
 import LandingPage from "../pages/public/home/landing.page";
 import EditProfilePage from "../pages/public/edit-profile.page";
-
+import SignInLayout from "../layout/SignInLayout";
+import LoginForm from "./../khoa/LoginForm";
 function App() {
   return (
     <Routes>
@@ -11,6 +12,10 @@ function App() {
         <Route path="edit-profile" element={<EditProfilePage />} />
         <Route path="about" element={<h1>about</h1>} />
         <Route path="dashboard" element={<h1>dashboard</h1>} />
+      </Route>
+      <Route element={<SignInLayout />}>
+        <Route path="/login" element={<LoginForm setAuth={"abc"} />} />
+        {/* <Route path="/signin" element={<SignIN />} /> */}
       </Route>
     </Routes>
   );
