e42bf1c829eb5dafcce3193027518431e1d56fe6
20120509 <topsiu1.ds@gmail.com>
2024-01-11 00:51:38 +0700

    sort course admin page

--- src/pages/admin/ui/courses-list.component.tsx
+++ src/pages/admin/ui/courses-list.component.tsx
@@ -1,23 +1,93 @@
 import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import { Grid, Pagination, Stack, TextField } from "@mui/material";
+import { Grid, IconButton, Pagination, Stack, TextField } from "@mui/material";
 import useAllCourses from "@/hooks/all-courses.hook";
 import CourseComponent from "./course.component";
 import { ChangeEvent, useState } from "react";
+import SortByAlphaIcon from '@mui/icons-material/SortByAlpha';
+import { inPlaceSort } from 'fast-sort';
 
 interface Props {
 }
 
 const CourseListComponent: React.FC<Props> = () => {
 
-    const { courses } = useAllCourses();
+    const { courses, coursesMutate } = useAllCourses();
     const [currentPageNumber, setCurrentPageNumber] = useState(1);
 
     const coursesPagination = courses?.slice((currentPageNumber - 1)*15, currentPageNumber*15)
 
     const handlePageChange = (event: ChangeEvent<unknown>, pageNumber: number) => {
         setCurrentPageNumber(pageNumber);
+    }
+
+    const [sortName, setSortName ] = useState(false);
+    const [sortDescription, setSortDescription ] = useState(false);
+    const [sortCourseOwner, setSortCourseOwner ] = useState(false);
+    const [sortInviteCode, setSortInviteCode ] = useState(false);
+
+    const handleSortClick = (attribute: string, state: boolean) => {
+        if (!state) {
+            switch (attribute) {
+                case "name":
+                    inPlaceSort(courses).asc([
+                        course => course.name,
+                    ]);
+                    setSortName(true);
+                    break;
+                case "description":
+                    inPlaceSort(courses).asc([
+                        course => course.description,
+                    ]);
+                    setSortDescription(true);
+                    break;
+                case "courseOwner":
+                    inPlaceSort(courses).asc([
+                        course => course.courseOwner,
+                    ]);
+                    setSortCourseOwner(true);
+                    break;
+                case "inviteCode":
+                    inPlaceSort(courses).asc([
+                        course => course.inviteCode,
+                    ]);
+                    setSortInviteCode(true);
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            switch (attribute) {
+                case "name":
+                    inPlaceSort(courses).desc([
+                        course => course.name,
+                    ]);
+                    setSortName(false);
+                    break;
+                case "description":
+                    inPlaceSort(courses).desc([
+                        course => course.description,
+                    ]);
+                    setSortDescription(false);
+                    break;
+                case "courseOwner":
+                    inPlaceSort(courses).desc([
+                        course => course.courseOwner,
+                    ]);
+                    setSortCourseOwner(false);
+                    break;
+                case "inviteCode":
+                    inPlaceSort(courses).desc([
+                        course => course.inviteCode,
+                    ]);
+                    setSortInviteCode(false);
+                    break;
+                default:
+                    break;
+            }
+        }
+        console.log(courses);
     }
 
     return (
@@ -30,7 +100,7 @@ const CourseListComponent: React.FC<Props> = () => {
                 label="Tìm kiếm"
                 type="search"
                 onInput={() => console.log("abc")}
-                sx={{ width: "400px" }}
+                sx={{ width: "400px", mb: 2 }}
                 />
             </Box>
             <Box
@@ -44,36 +114,60 @@ const CourseListComponent: React.FC<Props> = () => {
             >
                 <Grid container >
                     <Grid xs={3} item>
-                        <Typography
-                            variant="h6"
-                            sx={{ marginLeft: 1, color: "rgb(25,103,210)" }}
-                            >
-                            {`Tên môn học`}
-                        </Typography>
+                        <Stack direction={"row"}>
+                            <Typography
+                                variant="h6"
+                                sx={{ marginLeft: 1, color: "rgb(25,103,210)" }}
+                                >
+                                {`Tên môn học`}
+                            </Typography>
+                            <IconButton onClick={() => handleSortClick("name", sortName)}
+                            sx={{mt: "-4px"}}>
+                                <SortByAlphaIcon />
+                            </IconButton>
+                        </Stack>
                     </Grid>
                     <Grid xs={3} item>
-                        <Typography
-                            variant="h6"
-                            sx={{ marginLeft: 2, color: "rgb(25,103,210)" }}
-                            >
-                            {`Miêu tả`}
-                        </Typography>
+                        <Stack direction={"row"}>
+                            <Typography
+                                variant="h6"
+                                sx={{ marginLeft: 2, color: "rgb(25,103,210)" }}
+                                >
+                                {`Miêu tả`}
+                            </Typography>
+                            <IconButton onClick={() => handleSortClick("description", sortDescription)}
+                            sx={{mt: "-4px"}}>
+                                <SortByAlphaIcon />
+                            </IconButton>
+                        </Stack>
                     </Grid>
                     <Grid xs={3} item>
-                        <Typography
-                            variant="h6"
-                            sx={{ marginLeft: 2, color: "rgb(25,103,210)" }}
-                            >
-                            {`Giảng viên`}
-                        </Typography>
+                        <Stack direction={"row"}>
+                            <Typography
+                                variant="h6"
+                                sx={{ marginLeft: 2, color: "rgb(25,103,210)" }}
+                                >
+                                {`Giảng viên`}
+                            </Typography>
+                            <IconButton onClick={() => handleSortClick("courseOwner", sortCourseOwner)}
+                            sx={{mt: "-4px"}}>
+                                <SortByAlphaIcon />
+                            </IconButton>
+                        </Stack>
                     </Grid>
                     <Grid xs={3} item>
-                        <Typography
-                            variant="h6"
-                            sx={{ marginLeft: 2, color: "rgb(25,103,210)" }}
-                            >
-                            {`Mã mời`}
-                        </Typography>
+                        <Stack direction={"row"}>
+                            <Typography
+                                variant="h6"
+                                sx={{ marginLeft: 2, color: "rgb(25,103,210)" }}
+                                >
+                                {`Mã mời`}
+                            </Typography>
+                            <IconButton onClick={() => handleSortClick("inviteCode", sortInviteCode)}
+                            sx={{mt: "-4px"}}>
+                                <SortByAlphaIcon />
+                            </IconButton>
+                        </Stack>
                     </Grid>
                 </Grid>
             </Box>
