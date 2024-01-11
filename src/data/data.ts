d04a4aa318564e752a26416b271a1bce435ecfd9
20120509 <topsiu1.ds@gmail.com>
2024-01-12 01:21:03 +0700

    fix build

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,12 +1,11 @@
 import image8 from "../assets/images/khanh.png";
 import image7 from "../assets/images/khang.png";
 import image1 from "../assets/images/jack.png";
-import { IMember } from "../models/member.model";
 import UserModel from "@/models/user.model";
 
 export const members: UserModel[] = [
   {
-    phone_number: null,
+    phoneNumber: null,
     isSuspended: null,
     isBlocked: null,
     studentOfficialId: null,
@@ -20,7 +19,7 @@ export const members: UserModel[] = [
     bio: "Khang, tư duy sắc bén, đam mê mã nguồn mở và lập trình. Sở thích của anh không chỉ giải quyết vấn đề mà còn nghiên cứu và áp dụng công nghệ mới.",
   },
   {
-    phone_number: null,
+    phoneNumber: null,
     isSuspended: null,
     isBlocked: null,
     studentOfficialId: null,
@@ -34,7 +33,7 @@ export const members: UserModel[] = [
     bio: "Khanh, người sáng tạo và năng động, tận hưởng niềm vui lập trình và đam mê mã nguồn mở để thách thức bản thân và đóng góp cho cộng đồng.",
   },
   {
-    phone_number: null,
+    phoneNumber: null,
     isSuspended: null,
     isBlocked: null,
     studentOfficialId: null,
